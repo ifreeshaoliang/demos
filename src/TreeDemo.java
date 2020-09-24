43422fc41c4b74d646a8e124994e07cb24dc8c2b
ifreeshaoliang <ifreeshaoliang@163.com>
2020-09-24 15:44:53 +0800

    commit

---
+++ src/TreeDemo.java
@@ -0,0 +1,28 @@
+/**
+ * @author ifree
+ * @version 1.0
+ * @description: TODO
+ * @date 2020/9/21 19:54
+ */
+class TreeNode {
+    int val;
+    TreeDemo left;
+    TreeDemo right;
+    TreeNode(int x){
+        val = x;
+    }
+}
+
+class Tree {
+    TreeDemo root;
+    Tree(){
+        root = null;
+    }
+    void createTree(){
+        
+    }
+}
+
+public class TreeDemo {
+
+}
