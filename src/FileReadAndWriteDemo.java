883b9f2a9f70a27185b9c192504c53961964cfdc
ifreeshaoliang <ifreeshaoliang@163.com>
2020-10-20 21:32:11 +0800

    add file

---
+++ src/FileReadAndWriteDemo.java
@@ -0,0 +1,12 @@
+/**
+ * @author ifree
+ * @version 1.0
+ * @description: TODO
+ * @date 2020/10/20 9:36
+ */
+
+public class FileReadAndWriteDemo {
+    public static void main(String[] args) {
+
+    }
+}
